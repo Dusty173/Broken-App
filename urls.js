b9233021bb5ae72fd64f95fa3fd194a30e49f222
Dustin <dustincheyney@gmail.com>
2024-03-30 17:58:15 -0700

    Part 2 Completed

--- urls.js
+++ urls.js
@@ -1,51 +1,73 @@
 const fs = require('fs');
 const process = require('process');
-const axios = require('axios');
+const path = require('path');
+const http = require('http');
+const https = require('https');
 
-function read(path){
-    fs.readFile(path, 'utf8', (err, data) => {
-        if(err){
-            console.error(`Unable to read at ${path} Error: ${err}`);
-            process.exit(1);
-        } else{
-            console.log(data);
+// Checking for filename input
+if (process.argv.length !== 3) {
+    console.error('Usage: node urls.js <FILENAME>');
+    process.exit(1);
+  }
+
+// Reading contents of file.
+let file = process.argv[2];
+let urls;
+    
+try{
+    urls = fs.readFileSync(file, 'utf-8').split(/\r?\n/).filter(Boolean);
+    
+    console.log(urls)
+} catch {
+    console.error(`Unable to read at ${file}, ${err}`);
+    process.exit(1);
+}
+
+
+// Download content from URL and create new file.
+function downloadContent(url) {
+    const protocol = url.startsWith('https') ? https : http;
+    const options = { method: 'GET', headers: { 'User-Agent': 'Chrome/123.0.0.0' } };
+    return new Promise((resolve, reject) => {
+      protocol.get(url, options, (res) => {
+        let data = '';
+        res.on('data', (chunk) => { data += chunk; });
+        res.on('end', () => { resolve(data); });
+      }).on('error', (err) => { reject(err); });
+    });
+  }
+  
+
+// Write to a file 
+function writeToFile(filename, content) {
+    return new Promise((resolve, reject) => {
+      fs.writeFile(filename, content, 'utf-8', (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
         }
-    })
-}
+      });
+    });
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let path;
-let out;
-if(process.argv[2] === '--out'){
-    out= process.argv[3];
-    path = process.argv[4];
-} else {
-    path = process.argv[2];
-}
-
-if(path.slice(0,4) === 'http'){
-    webCat(path, out);
-} else {
-    read(path, out);
-}
+// Download all URLs and write to Files
+async function create() {
+    const promises = urls.map(async (url) => {
+      const hostname = new URL(url).hostname;
+      const filename = path.join(process.cwd(), hostname);
+  
+      try {
+        console.log(`Downloading ${url} to ${filename}`);
+        const content = await downloadContent(url);
+        await writeToFile(filename, content);
+        console.log(`Wrote to ${hostname}`);
+      } catch (err) {
+        console.error(`Error processing ${url}:`, err.message);
+      }
+    });
+  
+    await Promise.all(promises);
+  }
+  
+  create();
